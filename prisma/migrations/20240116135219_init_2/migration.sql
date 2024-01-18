86b815d3c07d3c89b0deb9e0d00c12e6263517ed
Turbobot <turbobot@vercel.com>
2024-01-18 20:25:11 +0530

    feat(create-turbo): apply official-starter transform

--- prisma/migrations/20240116135219_init_2/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Friend" ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'pending';
--- src/app/private/layout.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function AuthenticationLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="h-screen w-screen bg-black">{children}</div>;
-}
